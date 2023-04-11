d369fa6de500ab0866167edaec2f7eddb9b5a8d2
Munshi Muntasir Rifat <www.muntasirrifat23@gmail.com>
2023-04-11 17:32:28 +0600

    job

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './Component/Nav/Nav';
 import Header from './Component/Header/Header';
+import Catagory from './Component/Catagory/Catagory';
 
 function App() {
   return (
     <div className="App">
       <Nav></Nav>
       <Header></Header>
+      <Catagory></Catagory>
     </div>
   );
 }
---
+++ src/Component/Catagory/Catagory.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Catagory.css';
+const Catagory = () => {
+    return (
+        <div className='job'>
+            <div>
+                <h1>Jobs Category List</h1>
+                <h4>See how Many Jobs Are Available Now</h4>
+            </div>
+        </div>
+    );
+};
+
+export default Catagory;
